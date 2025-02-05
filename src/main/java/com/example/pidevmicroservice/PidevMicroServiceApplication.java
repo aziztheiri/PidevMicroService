bc65107554b5821a089e436de1ba8863bac3674e
aziztheiri <alatheiri123@gmail.com>
2025-02-05 09:10:48 +0100

    first commit

---
+++ src/main/java/com/example/pidevmicroservice/PidevMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pidevmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidevMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PidevMicroServiceApplication.class, args);
+    }
+
+}
