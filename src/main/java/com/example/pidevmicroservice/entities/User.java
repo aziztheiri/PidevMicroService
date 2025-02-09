8d9b9b3b5867b915a23709e911311ac1bdbdd4fa
aziztheiri <alatheiri123@gmail.com>
2025-02-09 11:43:39 +0100

    sign up

--- src/main/java/com/example/pidevmicroservice/entities/User.java
+++ src/main/java/com/example/pidevmicroservice/entities/User.java
@@ -19,7 +19,6 @@ public class User {
     private String email;
     private String password;
     private String name;
-    @Enumerated(EnumType.STRING)
     private UserRole userRole;
     private boolean isVerified;
     private String image;
