140a9f05be0dbc902bdb28af2ee67f48cdc4f971
raeftabarkin <mohamedraef.tabarki@esprit.tn>
2025-02-13 10:10:57 +0100

    test

---
+++ src/main/java/tn/esprit/pidevmicro/PidevMicroApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.pidevmicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidevMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PidevMicroApplication.class, args);
+    }
+
+}
